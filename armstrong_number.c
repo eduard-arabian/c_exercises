e12f7fa83c024b7d2a0999f30b7a9b7c4c2f65bd
eduard-arabian <eduard.arabian@gmail.com>
2022-07-16 23:53:08 +0300

    armstorng

---
+++ armstrong_number.c
@@ -0,0 +1,36 @@
+// Copyright (c) 2022. Eduard Arabian.
+
+#include <stdbool.h>
+#include <stdio.h>
+
+
+bool is_armstrong_number(int candidate) {
+    int armstrong = candidate;
+    int check = candidate;
+    int length = !armstrong;
+
+    while (armstrong) {
+        length++;
+        armstrong /= 10;
+    }
+
+    for (int i = length; i > 0; i--) {
+        int digit = check % 10;
+        int power = 1;
+        check /= 10;
+        for (int j = length; j > 0; j--)
+            power *= digit;
+        armstrong += power;
+    }
+
+    return (candidate == armstrong);
+}
+
+int main() {
+    int number;
+
+    scanf("%d", &number);
+    printf("%d", is_armstrong_number(number));
+
+    return 0;
+}
