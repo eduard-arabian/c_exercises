39280794ef232492cfef913bba1abc0f003e93ed
Eduard Arabian <eduard.arabian@gmail.com>
2022-07-29 00:44:24 +0300

    matrix

--- matrix_multiplication.c
+++ matrix_multiplication.c
@@ -19,7 +19,6 @@ int main() {
             C[i][j] = 0;
         }
     }
-
     struct timeval start, end;
     gettimeofday(&start, NULL);
 
@@ -49,3 +48,4 @@ float tdiff(struct timeval *start, struct timeval *end) {
     return (end->tv_sec - start->tv_sec)
         + 1e-6 * (end->tv_usec - start->tv_usec);
 }
+
