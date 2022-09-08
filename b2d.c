8f12807ae39fdab6e64977f843af177fac17471f
Eduard Arabian <eduard.arabian@gmail.com>
2022-09-08 14:50:13 +0300

    bin2dec und dec2bin tests

--- b2d.c
+++ b2d.c
@@ -2,22 +2,30 @@
 #include <math.h>
 
 long long convertBinaryToDecimal(long long bin);
+long long convertDecimalToBinary(int dec);
 
 int main(){
     long long bin;
     long long decimal;
+    int dec;
 
     printf("Input binary number: ");
     scanf("%lld", &bin);
     decimal = convertBinaryToDecimal(bin);
     if(decimal < 0)
-        printf("That's not a binary.\n");
+        printf("That's not a binary or number is too long.\n");
     else
         printf("%lld\n", decimal);
+
+    printf("Input decimal number: ");
+    scanf("%d", &dec);
+    decimal = convertDecimalToBinary(dec);
+    printf("%lld\n", decimal);
+
     return 0;
 }
 
-long long convertBinaryToDecimal(long long bin) {
+long long convertBinaryToDecimal(long long bin){
     int digit;
     long long bi = bin / 10;
     long long decimal = 0;
@@ -40,5 +48,20 @@ long long convertBinaryToDecimal(long long bin) {
     }
 
     return decimal;
+}
+
+long long convertDecimalToBinary(int dec){
+    long long bin = 0;
+    int digit;
+    long long pos = 1;
+
+    for(; dec > 0;){
+        digit = dec % 2;
+        dec /= 2;
+        bin += digit * pos;
+        pos *= 10;
+    }
+
+    return bin;
 }
 
